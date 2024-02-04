d1974786c58ab5cc92e676e93946adde403aa7dd
Tung ViO <tung@letdiv.com>
2024-02-04 11:24:00 +0700

    Doi sang TOP 3

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
 // Hiển thị giao diện trang Danh sách sản phẩm
 // Thêm chức năng gợi ý sản phẩm nếu người dùng đã đăng nhập
 // So sánh các sản phẩm
-// Hiển thị sản phẩm TOP 8
+// Hiển thị sản phẩm TOP 3

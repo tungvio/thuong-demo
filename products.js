693c1255249f4955b642bf49414a6182c02e6a34
Tung ViO <tung@letdiv.com>
2024-02-04 11:45:47 +0700

    update products

--- products.js
+++ products.js
@@ -1,4 +1,5 @@
 // Hiển thị giao diện trang Danh sách sản phẩm
 // Thêm chức năng gợi ý sản phẩm nếu người dùng đã đăng nhập
 // So sánh các sản phẩm
-// Hiển thị sản phẩm TOP 3
+// Hiển thị sản phẩm TOP 5
+// Hiển thị khuyến mãi
